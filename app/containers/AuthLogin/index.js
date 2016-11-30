49224e6d4b41f115d4774a6c291bfaf4a0d253f8
SakuB <841105127@qq.com>
2016-11-30 21:36:44 +0800

    add main page

--- app/containers/AuthLogin/index.js
+++ app/containers/AuthLogin/index.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-
+import { browserHistory } from 'react-router';
+import QueueAnim from 'rc-queue-anim';
 import styles from './styles.css';
 import CanvasBg from './CanvasBg';
 
 class AuthLogin extends Component {   // eslint-disable-line   react/prefer-stateless-function
+  handleOpenMainPage = () => {
+    browserHistory.push('/index');
+  }
   render() {
     return (
       <div style={{ width: '100%', height: '100%' }}>
         <CanvasBg />
-        <div className={styles.auth_cont}>
-          <h3>Open source blog</h3>
-          <div className={styles.auth_form}>
+        <QueueAnim type="bottom" className={styles.auth_cont}>
+          <h3 key="ani1">Open source blog</h3>
+          <div className={styles.auth_form}  key="ani2">
             <input className={styles.auth_input} placeholder="please input your account" />
             <span className={styles.error_input}>error</span>
             <input className={styles.auth_input} placeholder="please input your password" />
             <span className={styles.error_input}></span>
-            <button className={styles.login_btn}>Login</button>
+            <button
+              onTouchTap={this.handleOpenMainPage}
+              className={styles.login_btn}
+            >
+              Login
+            </button>
           </div>
-        </div>
+        </QueueAnim>
       </div>
     );
   }
---
+++ app/containers/MainPage/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+  render(){
+    return (
+      <div>
+        this is main !
+      </div>
+    );
+  }
+}
+
+export default MainPage;
