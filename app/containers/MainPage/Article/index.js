2dda1468b2ed9b0a8c988391fa09c78fd528ac78
SakuB <841105127@qq.com>
2016-12-02 14:49:45 +0800

    add searchbar

--- app/containers/MainPage/Article/index.js
+++ app/containers/MainPage/Article/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import HyperDown from 'hyperdown';
 
 import styles from './styles.css';
+const parser = new HyperDown();
 
 class Article extends Component {
+  state = {
+    html: '',
+  }
+  handleChangeContent = (e) => {
+    this.setState({ html: parser.makeHtml(e.target.value) });
+  }
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.article}>
-          <h3>測試標題啊</h3>
+          <div className={styles.search}>
+            <input placeholder="search some?" />
+          </div>
+          <div className={styles.article_list}>
+            <div className={styles.article_item} >
+              <div
+                className={styles.markdownBody}
+                // dangerouslySetInnerHTML={{ __html: this.state.html }}
+              >
+                大大四大神兽所所所所所所所所所所所所所所所所所所
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
