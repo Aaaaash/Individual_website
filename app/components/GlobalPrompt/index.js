0fe2714bb92d4a2e49bdb0af5681782b7c745f33
SakuB <841105127@qq.com>
2017-01-05 11:05:55 +0800

    修改提示框关闭动画

--- app/components/GlobalPrompt/index.js
+++ app/components/GlobalPrompt/index.js
@@ -26,10 +26,14 @@ const Prompt = styled.div`
   top: 0.15rem;
   padding: 0.15rem;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-  transition: all 450ms;
+  transition: all 250ms;
   animation-fill-mode:forwards;
   animation: ${transformOpen} 0.25s;
   z-index: 10000;
+  transform: ${(props) => props.close ?
+    'translateX(30px) rotateY(-45deg)' :
+    ''};
+  opacity: ${(props) => props.close ? 0 : ''};
 `;
 
 const Message = styled.p`
@@ -45,7 +49,9 @@ class GlobalPrompt extends Component {
     timeout: this.props.timeout,
     type: this.props.type,
     message: this.props.message,
+    close: false,
   }
+
   componentWillReceiveProps(nextProps) {
     this.setState({
       open: nextProps.open,
@@ -55,15 +61,24 @@ class GlobalPrompt extends Component {
     });
     if (this.props.open !== nextProps.open) {
       setTimeout(() => {
-        this.props.notificationCallback({ open: false });
+        this.handleClosePrompt();
       }, this.state.timeout);
     }
   }
+
+  handleClosePrompt = () => {
+    this.setState({ close: true });
+    setTimeout(() => {
+      this.props.notificationCallback({ open: false });
+      this.setState({ close: false });
+    }, 250);
+  }
+
   render() {
     return (
       <div>
         {this.state.open ?
-          <Prompt type={this.state.type} open={this.state.open}>
+          <Prompt type={this.state.type} close={this.state.close}>
             <Message><i className="fa fa-trash"></i>{this.state.message}</Message>
           </Prompt> :
         null}
